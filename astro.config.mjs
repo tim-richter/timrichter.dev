3f707edd9a9714d40e5d1412aba419666b18fa17
Tim Richter <tim@timrichter.dev>
2023-07-13 20:09:39 +0200

    build: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx(), tailwind({
-    config: {
-      applyBaseStyles: false
-    }
+    applyBaseStyles: false
   })]
 });
